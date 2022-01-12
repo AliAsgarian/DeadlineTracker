6eb8eaf74e226dafa6f6f250e9b8035112a185ab
aasgaria <aasgaria@uoguelph.ca>
2022-01-12 01:52:12 -0500

    updated imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,9 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter as Router, Route} from "react-router-dom";
 
 
-import Navbar from "./components/Navbar.component"
-import DeadlinesList from "./components/DeadlinesList.component";
-import EditDeadline from "./components/EditDeadline.component";
+import Navbar from "./components/navbar.component"
+import DeadlinesList from "./components/deadlinesList.component";
+import EditDeadline from "./components/editDeadline.component";
 import AddDeadline from "./components/AddDeadline.component";
 import AddCourse from "./components/AddCourse.component";
 
